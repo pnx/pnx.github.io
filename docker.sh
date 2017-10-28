b6673e0c2449f667e75d2877a42a4b06cdd4693b
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2017-10-28 15:39:35 +0200

    Adding docker script.

---
+++ docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export JEKYLL_VERSION=3.5
+
+docker run --rm \
+  --volume=$PWD:/srv/jekyll \
+  -p 4000:4000 \
+  -it jekyll/builder:$JEKYLL_VERSION \
+  jekyll serve --watch -H 0.0.0.0
