990e746247560c40735c0a6e46cde72d06dd78e0
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2022-09-07 17:44:47 +0200

    Remvoe docker script.

--- docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export JEKYLL_VERSION=3.5
-
-docker run --rm \
-  --volume=$PWD:/srv/jekyll \
-  -p 4000:4000 \
-  -it jekyll/builder:$JEKYLL_VERSION \
-  jekyll serve --watch -H 0.0.0.0
