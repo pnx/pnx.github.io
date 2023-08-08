3a3556827120593e6c488b08b614a6110e1d1c3f
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2023-08-08 18:53:43 +0200

    .vitepress/theme/Components/TwitterLink.vue: use FontAwesome component.

--- .vitepress/theme/Components/TwitterLink.vue
+++ .vitepress/theme/Components/TwitterLink.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import TwitterIcon from './Icons/TwitterIcon.vue';
 const props = defineProps<{
   username: string
 }>();
@@ -7,7 +6,7 @@ const props = defineProps<{
 
 <template>
     <a target="_blank" :href="'https://twitter.com/' + props.username">
-        <span class="icon icon--github"><TwitterIcon /></span>
+        <span class="icon"><font-awesome-icon icon="fa-brands fa-twitter" size="lg" /></span>
         <span class="username">{{ props.username }}</span>
     </a>
 </template>
