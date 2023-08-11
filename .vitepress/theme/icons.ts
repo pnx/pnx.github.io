4564b4d0df7939c0c8c8da22cfb7acd21f3ab447
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2023-08-11 13:15:42 +0200

    .vitepress/theme/icons.ts: Include more icons

--- .vitepress/theme/icons.ts
+++ .vitepress/theme/icons.ts
@@ -1,7 +1,31 @@
-
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+// Brand
+import {
+    faGithub,
+    faTwitter,
+    faPhp,
+    faLaravel,
+    faCss3,
+    faSass,
+    faLess,
+    faHtml5,
+    faVuejs,
+    faGolang,
+    faLinux,
+    faGit,
+} from '@fortawesome/free-brands-svg-icons';
+
+// Regular
 import { faClock } from '@fortawesome/free-regular-svg-icons';
+
+// Solid
 import { faTags } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faGithub, faTwitter, faClock, faTags)
+// Custom
+import faRedis from './icons/redis';
+import faMysql from './icons/mysql';
+import faCpp from './icons/cpp';
+
+library.add(faGithub, faTwitter, faPhp, faLaravel, faCss3, faSass, faLess,
+    faHtml5, faVuejs, faGolang, faLinux, faGit, faRedis, faMysql, faCpp, faClock, faTags);
