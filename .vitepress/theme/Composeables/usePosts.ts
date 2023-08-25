4f178e4d8ca3557a5da9670ca278b832bfd2edd6
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2023-08-25 11:22:28 +0200

    .vitepress/theme/Composeables/usePosts.ts: Adding category parameter.

--- .vitepress/theme/Composeables/usePosts.ts
+++ .vitepress/theme/Composeables/usePosts.ts
@@ -2,6 +2,10 @@
 import { data } from './posts.data.js'
 import { type Post } from '../types.js'
 
-export function usePosts() : Post[] {
+export function usePosts(category?: string | null) : Post[] {
+
+    if (category) {
+        return data.filter(post => post.tags?.indexOf(category) > -1)
+    }
     return data
 }
