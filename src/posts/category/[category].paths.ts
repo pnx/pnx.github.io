10c29ed28b03690387eef4fea629588fbf64cdef
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2023-08-25 12:13:45 +0200

    src/posts/category/[category].paths.ts: remove some debug stuff.

--- src/posts/category/[category].paths.ts
+++ src/posts/category/[category].paths.ts
@@ -6,17 +6,12 @@ export default {
     async paths() {
         const onlyUnique = (value : any, index : number, array : any) => array.indexOf(value) == index;
 
-        console.log("Loaded");
-
         // This is some crazy stuff :)
-        const c = globSync('src/posts/**/*.md')
+        return globSync('src/posts/**/*.md')
             .map(file => fm<any>(fs.readFileSync(file, "utf-8")).attributes.categories)
             .filter(c => c !== undefined)
             .flatMap(cat => cat)
             .filter(onlyUnique)
             .flatMap(cat => [ { params: { category: cat } } ]);
-
-            console.log(c);
-            return c;
     },
 }
