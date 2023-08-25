45839205b3a39b781e95ae922a7e2f520a4ec87e
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2023-08-25 11:24:59 +0200

    .vitepress/theme/Pages/BlogIndex.vue: support filtering by category

--- .vitepress/theme/Pages/BlogIndex.vue
+++ .vitepress/theme/Pages/BlogIndex.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 
 import { usePosts } from '../Composeables/usePosts'
-
+import { useData } from 'vitepress';
 import BlogListItem from './Partials/BlogListItem.vue';
 
+const { page } = useData();
 </script>
 
 <template>
+    <h2 v-if="page.params?.category">
+        Blog posts regarding {{ page.params?.category }}
+    </h2>
+
     <ul class="blog-list">
-        <BlogListItem v-for="post in usePosts()" v-bind:key="post.url" :post="post" />
+        <BlogListItem v-for="post in usePosts(page.params?.category)" v-bind:key="post.url" :post="post" />
     </ul>
 </template>
