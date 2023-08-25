5f6c83aba220525e0b9f89819d349344230ecda0
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2023-08-25 11:36:21 +0200

    .vitepress/theme/Pages/Partials/BlogPostMeta.vue: Add links for tags.

--- .vitepress/theme/Pages/Partials/BlogPostMeta.vue
+++ .vitepress/theme/Pages/Partials/BlogPostMeta.vue
@@ -23,7 +23,7 @@ defineProps<{
     <div v-if="post.tags && post.tags.length > 0" class="blog-post-meta-item">
         <span class="icon"><font-awesome-icon icon="fa-solid fa-tags" rotation="90" /></span>
         <ul class="tag-list">
-            <li v-for="tag in post.tags" v-bind:key="tag">{{ tag }}</li>
+            <li v-for="tag in post.tags" v-bind:key="tag"><a :href="'/posts/category/' + tag">{{ tag }}</a></li>
         </ul>
     </div>
 </div>
