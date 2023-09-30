16e9d0d11962ee9e73b78b66d8c2e7658982de5a
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2023-09-30 13:18:26 +0200

    Adding .vitepress/theme/Components/Skill.vue

---
+++ .vitepress/theme/Components/Skill.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+defineProps<{
+  name: string
+  label?: string
+}>();
+</script>
+
+<template>
+<div class="skill-list-item">
+    <FontAwesomeIcon :icon="['fab', name]" size="6x" :class="name"/>
+    <span>{{ label ?? name }}</span>
+</div>
+</template>
+
+<style scoped>
+
+.php {
+    color: #8993be;
+}
+
+.laravel {
+    color: #F05340;
+}
+
+.redis {
+    color: #D82C20;
+}
+
+.mysql {
+    color: #00549D;
+}
+
+.css3 {
+    color: #264de4;
+}
+
+.sass {
+    color: #CD6799;
+}
+
+.less {
+    color: #1d365d;
+}
+
+.vuejs {
+    color: #34495E;
+}
+
+.html5 {
+    color: #e34c26;
+}
+
+.golang {
+    color: #00ADD8;
+}
+
+.cpp {
+    color: #00549D;
+}
+
+.git {
+    color: #F1502F;
+}
+
+</style>
